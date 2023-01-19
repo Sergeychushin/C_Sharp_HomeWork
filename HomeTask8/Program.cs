b4a6113cf769b19b44f8828e38de4cc12ea28bf0
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-19 10:21:15 +0300

    addHomeTask8

---
+++ HomeTask8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+while(count <= number){
+      if(number % 2 == 0) {
+        Console.Write($"{count} ");
+        count = count + 2;
+      }
+
+}
