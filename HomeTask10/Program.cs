86f2ec46cd415c296b4814588df649755c42482d
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-19 10:23:19 +0300

    addHomeTask10

---
+++ HomeTask10/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int a1 = number / 100 * 100;
+int a2 = number % 10;
+int num = (number - (a1 + a2)) / 10;
+
+Console.WriteLine($"Трехзначное число {number} -> {num}"); 
