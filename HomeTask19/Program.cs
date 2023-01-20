dd239ff3b268ff5b34a04aa00458044741920879
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-20 15:41:15 +0300

    add HomeTask 19

---
+++ HomeTask19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет, 12821 -> да, 23432 -> да
+
+Console.Clear();
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 10000 & number < 100000) {
+    int a1 = number / 10000;
+    int a2 = number % 10;
+    int b1 = number / 1000 % 10;
+    int b2 = b1 % 100 % 10;
+    if (a1 == a2 & b1 == b2) {
+        Console.Write("Да"); 
+        }
+    else {
+        Console.Write("Нет"); 
+        }
+}
+else {
+   Console.Write("Вы ввели неверное число");  
+}
