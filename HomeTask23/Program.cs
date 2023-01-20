5c93ed6afa725cfcac4b2db40576d6b3f747ed17
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-20 16:48:30 +0300

    add homeTask 23

---
+++ HomeTask23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+
+while(count <= number) {
+      Math.Pow(count, 3);
+      Console.Write($"{Math.Pow(count, 3)} ");
+      count++;
+ 
+}
+      
