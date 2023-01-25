9396d215c2469f235894e3a50838e95430ae9d2e
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-25 20:48:39 +0300

    add HomeTask27

---
+++ HomeTask27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while(number > 0)
+{
+    int a = number % 10;
+    sum = sum + a;
+    number = number /10;
+}
+Console.WriteLine($" число {number} -> {sum}");
