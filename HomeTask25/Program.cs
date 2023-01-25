9080a3018752e9f7f98c694202affdb8077e44f4
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-25 19:34:06 +0300

    add Hometask 25

---
+++ HomeTask25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//Нельзя использовать библиотеку Math!
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+
+Console.WriteLine("Введите число A:  ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число В:  ");
+int b = int.Parse(Console.ReadLine()!);
+
+Math.Pow(a, b);
+Console.Write($" число A: {a} B: {b} -> {Math.Pow(a, b)} ");
+
+ 
