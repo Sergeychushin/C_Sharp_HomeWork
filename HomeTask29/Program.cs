06b4628bbbbe0442bd74af0594e21fd5cd1b9afd
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-28 12:45:15 +0300

    HomeTask29 complete

--- HomeTask29/Program.cs
+++ HomeTask29/Program.cs
@@ -3,7 +3,27 @@
 //6, 1, 33 -> [6, 1, 33]
 
 
-Console.WriteLine("Введите числа через пробел:");
-var input = Console.ReadLine();
-string[] inputArray = input.Trim().Split(new Char[] {' ' });
+Console.WriteLine("Введите числа:");
+int[] array = ReadArray();
 
+PrintArray(array);
+
+int[] ReadArray()
+{
+    int[] result = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        result[i] = int.Parse(Console.ReadLine()!);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+   int count = array.Length;
+   for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
