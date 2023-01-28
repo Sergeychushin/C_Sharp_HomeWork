f30b912bc5a1c3c750b56d0cffb3fcec7d0720d5
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-28 15:12:51 +0300

    add HomeTask34

---
+++ HomeTask34/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int maxValue)
+
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(maxValue + 1);
+    }
+    return result;
+}
+
+int[] array = GetArray(8, 235);
+Console.WriteLine(String.Join(" ", array));
+
+int evenElCount = 0;
+
+foreach (int el in array)
+{
+    if (el % 2 == 0) 
+    {
+        evenElCount++;
+      
+    }
+        
+}
+Console.WriteLine($"Масиив = {String.Join(" ", array)} -> {evenElCount} ");
