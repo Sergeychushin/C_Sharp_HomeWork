40419268440b0d561f41177ca6652b898b0fe115
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-01-30 11:42:19 +0300

    add AddHomeTasj

---
+++ AddHomeTask/Program.cs
@@ -0,0 +1,14 @@
+﻿ //Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+
+
+Console.Write("Высота елки ");
+int H = Int32.Parse(Console.ReadLine());
+string Z = "*";
+for (int i = 0; i < H; i++)
+{
+    Console.WriteLine(Z);
+    Z += "**";
+
+}
+Console.ReadLine();
+
