9a7c1e12eeb3b5798a1a541ab23fe2600e754bc3
Sergei I Chushin <Sergei.Chushin@svetopaper.com>
2023-02-07 20:11:54 +0300

    add HomeTask52

---
+++ HomeTask52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+    Console.WriteLine(" ");
+}
+
+void FillArray(int[,] matr)
+{
+      for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); 
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    double avg = Math.Round(sum / matrix.GetLength(0), 1);
+    Console.Write($"{avg} ");
+    
+}
